b1c90f167d9346bebaceb17e62c153579c893841
Rob Quick <requick1031@gmail.com>
2018-08-02 13:00:07 +0900

    add three fragments
    SpotMaster, PartialMap, and RatedSpots fragments were added to be displayed upon bottom navigation selection.

--- App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/MainActivity.java
+++ App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/MainActivity.java
@@ -1,33 +1,46 @@
 package com.dhochmanrquick.skatespotorganizer;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.dhochmanrquick.skatespotorganizer.dummy.DummyContent;
 
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements
+        SpotMasterFragment.OnListFragmentInteractionListener,
+        PartialMapFragment.OnFragmentInteractionListener,
+        RatedSpotsFragment.OnFragmentInteractionListener{
+
+    private FragmentManager mFragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_my_spots);
-                    return true;
+                    selectedFragment = SpotMasterFragment.newInstance(1);
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_map);
-                    return true;
+                    selectedFragment = PartialMapFragment.newInstance();
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_rated);
-                    return true;
+                    selectedFragment = RatedSpotsFragment.newInstance();
+                    break;
             }
-            return false;
+            mFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+            return true;
         }
     };
 
@@ -36,8 +49,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SpotMasterFragment.newInstance(1))
+                .commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Toast.makeText(this,
+                "Spot number " + item + " has been selected",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // not sure if we'll need this interface, but let's keep it for now.
     }
 }
