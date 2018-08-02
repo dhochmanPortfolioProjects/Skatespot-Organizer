aa1d0e080288bcdaceba85fce8a8c52df58ed5dd
Rob Quick <requick1031@gmail.com>
2018-08-02 12:04:19 +0900

    add bottom navigation to main activity

--- App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/MainActivity.java
+++ App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/MainActivity.java
@@ -1,13 +1,43 @@
 package com.dhochmanrquick.skatespotorganizer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_my_spots);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_map);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_rated);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
