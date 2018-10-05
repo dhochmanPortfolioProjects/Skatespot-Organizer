3f85e74402fb68ca948fe293950f09b4a4cc8c11
Daniel Hochman <danielhochman1@yahoo.es>
2018-10-05 22:13:54 +0900

    Clean up code in data layer and add JDOC comments

--- App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/data/SpotRepository.java
+++ App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/data/SpotRepository.java
@@ -11,12 +11,11 @@ import java.util.List;
  * You use the Repository for managing multiple data sources.
  * This class mainly wraps the SQL-mapped Java methods defined in the DAO; it is effectively an
  * abstract DAO.
- *
+ * <p>
  * A Repository is a class that abstracts access to multiple data sources. The Repository is not part
  * of the Architecture Components libraries, but is a suggested best practice for code separation and
  * architecture. A Repository class handles data operations. It provides a clean API to the rest of
  * the app for app data.
- *
  *
  * @author Daniel Hochman
  * @author Rob Quick
@@ -26,40 +25,71 @@ public class SpotRepository {
     private SpotDao mSpotDao; // Member variable for the DAO
     private LiveData<List<Spot>> mAllSpots; // Member variable for the list of Spots. This caches the list of Spots.
 
-    // Constructor that gets a handle to the database and initializes the member variables.
+    /**
+     * Constructor that gets a handle to the database and initializes the member variables.
+     *
+     * @param application
+     */
     public SpotRepository(Application application) {
-        SpotRoomDatabase db = SpotRoomDatabase.getDatabase(application); // Get singleton instance of the database
-        mSpotDao = db.getSpotDao(); // Get the Dao for the database
-        mAllSpots = mSpotDao.getAll(); // Use the Dao to query the SQLite database and return the LiveData
+        // Get singleton instance of the database
+        SpotRoomDatabase spotRoomDatabase = SpotRoomDatabase.getDatabase(application);
+        mSpotDao = spotRoomDatabase.getSpotDao(); // Get the DAO for the Spot database from the SpotRoomDatabase
+        mAllSpots = mSpotDao.getAll(); // Use the DAO to query the SQLite database and return the LiveData
+        // This could be called in the method getAllSpots() when the spot list is actually needed
     }
 
-    // A wrapper for getSpot() defined in the DAO
+    /**
+     * A overloaded wrapper for getSpot() defined in the DAO.
+     *
+     * @param id ID of the Spot to be retrieved from the database
+     * @return The requested Spot or null if the Spot is not found
+     */
     public LiveData<Spot> getSpot(int id) {
         return mSpotDao.getSpot(id);
     }
 
-    // A wrapper for getSpot() defined in the DAO
+    /**
+     * An overloaded wrapper for getSpot() defined in the DAO.
+     *
+     * @param name Name of the Spot to be retrieved from the database
+     * @return The requested Spot or null if the Spot is not found
+     */
     public LiveData<Spot> getSpot(String name) {
         return mSpotDao.getSpot(name);
     }
 
-    // A wrapper for getAllSpots() defined in the DAO; effectively a getter method for the mAllSpots member variable.
+    /**
+     * A wrapper for getAllSpots() defined in the DAO; effectively a getter method for the mAllSpots
+     * member variable.
+     *
+     * @return The List of all Spots in the database, wrapped in LiveData
+     */
     public LiveData<List<Spot>> getAllSpots() {
         return mAllSpots;
     }
 
-    // A wrapper for the insert() method defined in the DAO. Must call this on a non-UI thread or your app will crash;
-    // Room ensures that you don't do any long-running operations on the main thread, blocking the UI.
-    public void insert (Spot spot) {
+    /**
+     * A wrapper for the insert() method defined in the DAO. Must call this on a non-UI thread or your
+     * app will crash; Room ensures that you don't do any long-running operations on the main thread,
+     * blocking the UI.
+     *
+     * @param spot The Spot to be inserted into the database
+     */
+    public void insert(Spot spot) {
+        // Create new insertAsyncTask and pass in the DAO to its constructor
         new insertAsyncTask(mSpotDao).execute(spot);
     }
 
-    // Private inner class
+    /**
+     * Private inner AsyncTask class to insert a Spot into the database, which must be done on a
+     * non-UI thread or your app will crash; Room ensures that you don't do any long-running
+     * operations on the main thread, blocking the UI.
+     */
     private static class insertAsyncTask extends AsyncTask<Spot, Void, Void> {
 
         private SpotDao mAsyncTaskDao; // Member variable to hold the DAO (passed into the constructor)
 
-        // Constructor: Stores the DAO passed in in the local mAsyncTaskDao
+        // Constructor: Stores the DAO passed in into the local variable mAsyncTaskDao
         insertAsyncTask(SpotDao dao) {
             mAsyncTaskDao = dao;
         }
--- App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/data/SpotRoomDatabase.java
+++ App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/data/SpotRoomDatabase.java
@@ -14,13 +14,15 @@ import com.google.android.gms.maps.model.LatLng;
 import java.util.Random;
 
 /**
- * The Room database class for the Spot SQLite table.
- *
- * Room is a database layer on top of an SQLite database that takes care of mundane tasks that you
- * used to handle with an SQLiteOpenHelper. RoomDatabase is a database holder that serves as an
- * access point to the underlying SQLite database. The Room database uses the DAO to issue queries
- * to the SQLite database. Your Room class must be abstract and extend RoomDatabase. Usually, you
- * only need one instance of the Room database for the whole app.
+ * The Room database class for the Spot SQLite table. This class's main purpose is to build the
+ * Room database (via Room.databaseBuilder()) and define and return the DAOs that work with the
+ * database (via getSpotDao()).
+ * <p>
+ * From the documentation: Room is a database layer on top of an SQLite database that takes care of
+ * mundane tasks that you used to handle with an SQLiteOpenHelper. RoomDatabase is a database holder
+ * that serves as an access point to the underlying SQLite database. The Room database uses the DAO
+ * to issue queries to the SQLite database. Your Room class must be abstract and extend RoomDatabase.
+ * Usually, you only need one instance of the Room database for the whole app.
  *
  * @author Daniel Hochman
  * @author Rob Quick
@@ -31,25 +33,25 @@ import java.util.Random;
 public abstract class SpotRoomDatabase extends RoomDatabase {
 
     // Define the DAOs that work with the database. Provide an abstract "getter" method for each @Dao.
-    // So far, the only DAO needed is SpotDao. This method returns that DAO.
+    // So far, the only DAO needed is SpotDao. This method returns that DAO; its implementation
+    // seems to be defined elsewhere by the Room library.
     public abstract SpotDao getSpotDao();
 
-    private static SpotRoomDatabase sSpotRoomDatabase; // The Singleton instance of this SpotRoomDatabase
-
+    private static SpotRoomDatabase sSpotRoomDatabase; // The singleton instance of this SpotRoomDatabase
 //    private static Context mContext;
 
     // A static (factory) method that returns the singleton instance of the database. The SpotRoomDatabase
     // is a singleton to prevent having multiple instances of the database opened at the same time.
-    // This method is called by SpotRepository's constructor
+    // This method is called by SpotRepository's constructor, mainly so that it can retrieve the SpotDao from it.
     public static SpotRoomDatabase getDatabase(final Context context) {
 //        mContext = context;
         if (sSpotRoomDatabase == null) { // If no instance of the database exists yet, create it and return it
             synchronized (SpotRoomDatabase.class) {
                 if (sSpotRoomDatabase == null) {
-                    // Create database here: This code uses Room's database builder to create a
+                    // Build database here: This code uses Room's database builder to create a
                     // RoomDatabase object in the application context from the SpotRoomDatabase
-                    // class and names it "spot_database".
-                    sSpotRoomDatabase = Room.databaseBuilder(context.getApplicationContext(),
+                    // class, and names it "spot_database".
+                    sSpotRoomDatabase = Room.databaseBuilder(context.getApplicationContext(), // Build database
                             SpotRoomDatabase.class, "spot_database")
                             // add the callback to the database build sequence right before calling .build().
 //                            .addCallback(sRoomDatabaseCallback)
@@ -58,6 +60,13 @@ public abstract class SpotRoomDatabase extends RoomDatabase {
             }
         }
         return sSpotRoomDatabase; // Else; return the already instantiated singleton SpotRoomDatabase object
+    }
+
+    // When using the database debug library, this method must be overridden; otherwise
+    // error: SpotRoomDatabase_Impl is not abstract and does not override abstract method clearAllTables() in RoomDatabase
+    @Override
+    public void clearAllTables() {
+        // TODO: 8/14/18 Implement method
     }
 
     // A RoomDatabase.Callback overriding onOpen() to delete all content and repopulate the database
@@ -113,11 +122,4 @@ public abstract class SpotRoomDatabase extends RoomDatabase {
 //            return null;
 //        }
 //    }
-
-    // When using the database debug library, this method must be overridden; otherwise
-    // error: SpotRoomDatabase_Impl is not abstract and does not override abstract method clearAllTables() in RoomDatabase
-    @Override
-    public void clearAllTables() {
-        // TODO: 8/14/18 Implement method
-    }
 }
