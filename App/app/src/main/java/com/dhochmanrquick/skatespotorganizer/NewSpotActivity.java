5b79ff42fd450886e50be7a538bf868140c5a71f
Daniel Hochman <danielhochman1@yahoo.es>
2018-10-06 17:09:58 +0900

    Add ImageView for Spot photo in NewSpotActivity
    When creating a new Spot, after taking a picture, the photo
    can now be previewed in the NewSpotActivity's layout.

--- App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/NewSpotActivity.java
+++ App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/NewSpotActivity.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.dhochmanrquick.skatespotorganizer.data.Spot;
 import com.dhochmanrquick.skatespotorganizer.data.SpotViewModel;
@@ -40,14 +42,15 @@ public class NewSpotActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_spot);
 
+        // Get the ViewModel to access the underlying database
+        mSpotViewModel = ViewModelProviders.of(this).get(SpotViewModel.class);
+
         Location location = getIntent().getParcelableExtra(EXTRA_CURRENT_LOCATION);
+
         if (location != null) {
             ((EditText) findViewById(R.id.new_spot_latitude)).setText("" + location.getLatitude());
             ((EditText) findViewById(R.id.new_spot_longtitude)).setText("" + location.getLongitude());
         }
-
-        // Get ViewModel (abstract data layer)
-        mSpotViewModel = ViewModelProviders.of(this).get(SpotViewModel.class);
 
         // Create camera/image capture implicit intent
         final Intent captureImage_Intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -73,6 +76,9 @@ public class NewSpotActivity extends AppCompatActivity {
                 Uri uri = FileProvider.getUriForFile(getApplicationContext(),
                         "com.dhochmanrquick.skatespotorganizer.fileprovider", mPhotoFile);
 
+                // If you pass the extra parameter MediaStore.EXTRA_OUTPUT with the camera intent
+                // then camera activity will write the captured image to that path and it will not
+                // return the bitmap in the onActivityResult method.
                 captureImage_Intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
 
                 // Query Package Manager for every activity cameraImage_Intent can resolve to
@@ -135,5 +141,45 @@ public class NewSpotActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    /**
+     * The Android Camera application encodes the photo in the return Intent delivered to
+     * onActivityResult() as a small Bitmap in the extras, under the key "data".
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param intent
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//        super.onActivityResult(requestCode, resultCode, intent);
+
+//        switch (requestCode) {
+//            case REQUEST_PHOTO:
+//                if (resultCode == RESULT_OK && intent.hasExtra("data")) {
+//                    Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+//                    if (bitmap != null) {
+//                        ((ImageView) findViewById(R.id.new_spot_photo_iv)).setImageBitmap(bitmap);
+////                        ivThumbnailPhoto.setImageBitmap(bitmap);
+//                    }
+//                }
+//        if (requestCode == REQUEST_PHOTO && resultCode == RESULT_OK && intent.hasExtra("data")) {
+////            Bundle extras = intent.getExtras();
+//            Bitmap imageBitmap = (Bitmap) intent.getExtras().get("data");
+//            ((ImageView) findViewById(R.id.new_spot_photo_iv)).setImageBitmap(imageBitmap);
+////                        ivThumbnailPhoto.setImageBitmap(bitmap);
+////                    mImageView.setImageBitmap(imageBitmap);
+////                break;
+//        }
+        File filesDir = getFilesDir(); // Get handle to directory for private application files
+        File photoFile = new File(filesDir, mNewSpot.getPhotoFilename()); // Create new File in the directory
+        Bitmap bitmap = PictureUtils.getScaledBitmap(photoFile.getPath(), 1000, 1000);
+//            Bitmap bitmap = PictureUtils.getScaledBitmap("/data/user/0/com.dhochmanrquick.skatespotorganizer/files/IMG_0.jpg", 50, 50);
+        ((ImageView) findViewById(R.id.new_spot_photo_iv)).setImageBitmap(bitmap);
+
+
+//                    ((ImageView) findViewById(R.id.new_spot_photo_iv)).setImageBitmap(imageBitmap);
+
     }
 }
