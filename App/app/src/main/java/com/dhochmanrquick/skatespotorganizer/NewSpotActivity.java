c08e6d1061c0a0560ef36d79bba5df61c86b0c7a
Daniel Hochman <danielhochman1@yahoo.es>
2018-10-15 17:45:19 +0900

    Add image source selection AlertDialog

--- App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/NewSpotActivity.java
+++ App/app/src/main/java/com/dhochmanrquick/skatespotorganizer/NewSpotActivity.java
@@ -38,7 +38,7 @@ public class NewSpotActivity extends AppCompatActivity {
 
     private Spot mNewSpot;
     private SpotViewModel mSpotViewModel;
-//    private static final int REQUEST_PHOTO = 2;
+    //    private static final int REQUEST_PHOTO = 2;
     private static final String EXTRA_CURRENT_LOCATION = "com.dhochmanrquick.skatespotorganizer.current_location";
     private boolean inEditMode = false;
     private boolean inCurrentLocationMode = false;
@@ -52,6 +52,74 @@ public class NewSpotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_spot);
+
+        // Configure image source selection AlertDialog
+        final String[] items = new String[]{"Take from camera", "Select from gallery"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, items);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Image");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) { // Pick from camera
+                if (item == 0) {
+                    // Create camera/image capture implicit intent
+                    final Intent captureImage_Intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    // Determine whether there is a camera app available
+                    boolean canTakePhoto = captureImage_Intent.resolveActivity(getPackageManager()) != null;
+                    // Create new Spot and populate its fields with text from the EditTexts
+                    // Todo: Add data validation
+                    mNewSpot = new Spot(((EditText) findViewById(R.id.new_spot_name)).getText().toString(),
+                            new LatLng(Double.parseDouble(((EditText) findViewById(R.id.new_spot_latitude)).getText().toString()),
+                                    Double.parseDouble(((EditText) findViewById(R.id.new_spot_longtitude)).getText().toString())),
+                            ((EditText) findViewById(R.id.new_spot_description)).getText().toString(),
+                            /*R.drawable.dangsan_spot_landscape*/ 0);
+
+                    File mPhotoFile = PictureUtils.getPhotoFile(getApplication(), mNewSpot);
+
+                    // Translate the local filepath stored in mPhotoFile into a Uri the camera app can see
+                    Uri uri = FileProvider.getUriForFile(getApplicationContext(),
+                            "com.dhochmanrquick.skatespotorganizer.fileprovider", mPhotoFile);
+
+                    // If you pass the extra parameter MediaStore.EXTRA_OUTPUT with the camera intent
+                    // then camera activity will write the captured image to that path and it will not
+                    // return the bitmap in the onActivityResult method.
+                    captureImage_Intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+                    // Query Package Manager for every activity cameraImage_Intent can resolve to
+                    List<ResolveInfo> cameraActivity =
+                            getPackageManager().queryIntentActivities(captureImage_Intent, PackageManager.MATCH_DEFAULT_ONLY);
+
+                    // Grant write permission for this Uri to each activity
+                    for (ResolveInfo activity : cameraActivity) {
+                        grantUriPermission(activity.activityInfo.packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                    }
+
+                    startActivityForResult(captureImage_Intent, PICK_FROM_CAMERA);
+//                    Intent intent 	 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//
+//                    mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+//                            "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+//
+//                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+//
+//                    try {
+//                        intent.putExtra("return-data", true);
+//
+//                        startActivityForResult(intent, PICK_FROM_CAMERA);
+//                    } catch (ActivityNotFoundException e) {
+//                        e.printStackTrace();
+//                    }
+                } else { // Pick from file
+                    Intent intent = new Intent();
+
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+                }
+            }
+        });
+        final AlertDialog dialog = builder.create();
+
 
         // Get the ViewModel to access the underlying database
         mSpotViewModel = ViewModelProviders.of(this).get(SpotViewModel.class);
@@ -181,17 +249,17 @@ public class NewSpotActivity extends AppCompatActivity {
         findViewById(R.id.new_spot_camera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dialog.show();
                 // Create camera/image capture implicit intent
 //                final Intent captureImage_Intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
 
-                Intent intent = new Intent();
-                // Show only images, no videos or anything else
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // Always show the chooser (if there are multiple options available)
-                startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+//                Intent intent = new Intent();
+//                // Show only images, no videos or anything else
+//                intent.setType("image/*");
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+//                // Always show the chooser (if there are multiple options available)
+//                startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
 
 //                final String [] items			= new String [] {"Take from camera", "Select from gallery"};
 //                ArrayAdapter<String> adapter	= new ArrayAdapter<String> (getBaseContext()/*this*/, android.R.layout.select_dialog_item, items);
@@ -229,8 +297,6 @@ public class NewSpotActivity extends AppCompatActivity {
 //                } );
 
 
-
-
 //
 //                // Determine whether there is a camera app available
 //                boolean canTakePhoto = captureImage_Intent.resolveActivity(getPackageManager()) != null;
@@ -263,8 +329,6 @@ public class NewSpotActivity extends AppCompatActivity {
 //                }
 //
 //                startActivityForResult(captureImage_Intent, REQUEST_PHOTO);
-
-
 
 
 //                mSpotViewModel.insert(mNewSpot);
@@ -302,16 +366,17 @@ public class NewSpotActivity extends AppCompatActivity {
                 break;
             case PICK_FROM_FILE:
                 mImageCaptureUri = intent.getData();
+//                mImageCaptureUri.get
+//                MediaStore.Images.Media.
                 try {
+                    // Create Bitmap from the return contentURI
                     bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mImageCaptureUri);
-                    // Log.d(TAG, String.valueOf(bitmap));
 
-                    ImageView imageView = (ImageView) findViewById(R.id.new_spot_photo_iv);
+                    ImageView imageView = findViewById(R.id.new_spot_photo_iv);
                     imageView.setImageBitmap(bitmap);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
 
 //                filesDir = getFilesDir(); // Get handle to directory for private application files
 //                photoFile = new File(filesDir, mImageCaptureUri.getPath()); // Create new File in the directory
@@ -338,9 +403,6 @@ public class NewSpotActivity extends AppCompatActivity {
         }
 
 
-
-
-
 //        switch (requestCode) {
 //            case REQUEST_PHOTO:
 //                if (resultCode == RESULT_OK && intent.hasExtra("data")) {
@@ -358,7 +420,6 @@ public class NewSpotActivity extends AppCompatActivity {
 ////                    mImageView.setImageBitmap(imageBitmap);
 ////                break;
 //        }
-
 
 
 //                    ((ImageView) findViewById(R.id.new_spot_photo_iv)).setImageBitmap(imageBitmap);
